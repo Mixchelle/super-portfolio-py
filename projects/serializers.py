d3c666fa4782c4378b8f2d54e2f6193e31bf7cf2
Mixchelle <michelle.biologia@hotmail.com>
2023-09-03 01:22:18 -0300

    requisito 5

--- projects/serializers.py
+++ projects/serializers.py
@@ -14,15 +14,34 @@ class ProjectSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class CertifyingInstitutionCertificateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = ["id", "name"]
+
+
 class CertificateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Certificate
-        fields = '__all__'
+        fields = "__all__"
 
 
 class CertifyingInstitutionSerializer(serializers.ModelSerializer):
-    certificates = CertificateSerializer(many=True, read_only=True)
+    certificates = CertifyingInstitutionCertificateSerializer(many=True)
 
     class Meta:
         model = CertifyingInstitution
-        fields = '__all__'
+        fields = ["id", "name", "url", "certificates"]
+
+    def create(self, validated_data):
+        data = validated_data.pop("certificates")
+        certifying_institution = CertifyingInstitution.objects.create(
+            **validated_data
+        )
+
+        for certificate in data:
+            Certificate.objects.create(
+                certifying_institution=certifying_institution, **certificate
+            )
+
+        return certifying_institution
